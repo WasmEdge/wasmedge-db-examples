142b0d3dba85011ee2898451d48ce30f26e81d5e
Richard Chien <stdrc@outlook.com>
2022-07-09 20:20:07 +0800

    add tokio version of send_tcp_message and receive_tcp_message
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/nodes/mod.rs
+++ src/nodes/mod.rs
@@ -1,75 +1,156 @@
 //! Abstractions for the node types, including the main types [`KvsNode`], [`RoutingNode`], and [`ClientNode`].
 
 pub use self::client::ClientNode;
-use crate::messages::TcpMessage;
-use eyre::{bail, Context};
-use futures::AsyncReadExt;
-use smol::{io::AsyncWriteExt, net::TcpStream};
-use std::convert::TryInto;
 
 pub mod client;
 
-/// Sends the given message on the given tcp stream.
-///
-/// TCP messages should only be sent using this method, to ensure that all
-/// messages are sent in the same format.
-pub async fn send_tcp_message(
-    message: &TcpMessage,
-    connection: &mut TcpStream,
-) -> eyre::Result<()> {
-    let serialized = serde_json::to_vec(&message).context("failed to serialize tcp message")?;
-    let len = (serialized.len() as u64).to_le_bytes();
-    connection
-        .write_all(&len)
-        .await
-        .context("failed to send message length")?;
-    connection
-        .write_all(&serialized)
-        .await
-        .context("failed to send message")?;
-    Ok(())
+mod legacy {
+    use crate::messages::TcpMessage;
+    use eyre::{bail, Context};
+    use futures::AsyncReadExt;
+    use smol::{io::AsyncWriteExt, net::TcpStream};
+    use std::convert::TryInto;
+
+    /// Sends the given message on the given tcp stream.
+    ///
+    /// TCP messages should only be sent using this method, to ensure that all
+    /// messages are sent in the same format.
+    pub async fn send_tcp_message(
+        message: &TcpMessage,
+        connection: &mut TcpStream,
+    ) -> eyre::Result<()> {
+        let serialized = serde_json::to_vec(&message).context("failed to serialize tcp message")?;
+        let len = (serialized.len() as u64).to_le_bytes();
+        connection
+            .write_all(&len)
+            .await
+            .context("failed to send message length")?;
+        connection
+            .write_all(&serialized)
+            .await
+            .context("failed to send message")?;
+        Ok(())
+    }
+
+    /// Receives a [`TcpMessage`] from the given stream.
+    ///
+    /// This function requires that all messages are sent using [`send_tcp_message`],
+    /// otherwise parsing the messages will fail.
+    pub async fn receive_tcp_message(
+        stream: &mut (impl futures::AsyncRead + Unpin),
+    ) -> eyre::Result<Option<TcpMessage>> {
+        const MAX_MSG_LEN: u64 = u32::MAX as u64;
+
+        let mut len_raw = [0; 8];
+        if let Err(err) = stream.read_exact(&mut len_raw).await {
+            if err.kind() == std::io::ErrorKind::UnexpectedEof
+                || err.kind() == std::io::ErrorKind::ConnectionReset
+            {
+                return Ok(None);
+            } else {
+                return Err(eyre::Error::new(err).wrap_err("failed to read message length"));
+            }
+        }
+        let len = u64::from_le_bytes(len_raw);
+
+        if len > MAX_MSG_LEN {
+            bail!("Message is too long (length: {} bytes)", len);
+        }
+
+        let mut buf = vec![0; len.try_into().unwrap()];
+        if let Err(err) = stream.read_exact(&mut buf).await {
+            if err.kind() == std::io::ErrorKind::UnexpectedEof {
+                log::warn!("receive tcp message failed: {}", err);
+                return Ok(None);
+            } else {
+                return Err(eyre::Error::new(err).wrap_err("failed to read message"));
+            }
+        }
+        serde_json::from_slice(&buf)
+            .with_context(|| {
+                format!(
+                    "failed to deserialize message: `{}`",
+                    String::from_utf8_lossy(&buf)
+                )
+            })
+            .map(Some)
+    }
 }
 
-/// Receives a [`TcpMessage`] from the given stream.
-///
-/// This function requires that all messages are sent using [`send_tcp_message`],
-/// otherwise parsing the messages will fail.
-pub async fn receive_tcp_message(
-    stream: &mut (impl futures::AsyncRead + Unpin),
-) -> eyre::Result<Option<TcpMessage>> {
-    const MAX_MSG_LEN: u64 = u32::MAX as u64;
+pub use legacy::*;
 
-    let mut len_raw = [0; 8];
-    if let Err(err) = stream.read_exact(&mut len_raw).await {
-        if err.kind() == std::io::ErrorKind::UnexpectedEof
-            || err.kind() == std::io::ErrorKind::ConnectionReset
-        {
-            return Ok(None);
-        } else {
-            return Err(eyre::Error::new(err).wrap_err("failed to read message length"));
+#[allow(dead_code)]
+mod tokio {
+    use eyre::{bail, Context};
+    use tokio::{
+        io::{AsyncReadExt, AsyncWriteExt},
+        net::TcpStream,
+    };
+
+    use crate::messages::TcpMessage;
+
+    /// Sends the given message on the given tcp stream.
+    ///
+    /// TCP messages should only be sent using this method, to ensure that all
+    /// messages are sent in the same format.
+    pub async fn send_tcp_message_tokio(
+        message: &TcpMessage,
+        connection: &mut TcpStream,
+    ) -> eyre::Result<()> {
+        let serialized = serde_json::to_vec(&message).context("failed to serialize tcp message")?;
+        let len = (serialized.len() as u64).to_le_bytes();
+        connection
+            .write_all(&len)
+            .await
+            .context("failed to send message length")?;
+        connection
+            .write_all(&serialized)
+            .await
+            .context("failed to send message")?;
+        Ok(())
+    }
+
+    /// Receives a [`TcpMessage`] from the given stream.
+    ///
+    /// This function requires that all messages are sent using [`send_tcp_message`],
+    /// otherwise parsing the messages will fail.
+    pub async fn receive_tcp_message(
+        stream: &mut (impl AsyncReadExt + Unpin),
+    ) -> eyre::Result<Option<TcpMessage>> {
+        const MAX_MSG_LEN: u64 = u32::MAX as u64;
+
+        let mut len_raw = [0; 8];
+        if let Err(err) = stream.read_exact(&mut len_raw).await {
+            if err.kind() == std::io::ErrorKind::UnexpectedEof
+                || err.kind() == std::io::ErrorKind::ConnectionReset
+            {
+                return Ok(None);
+            } else {
+                return Err(eyre::Error::new(err).wrap_err("failed to read message length"));
+            }
         }
-    }
-    let len = u64::from_le_bytes(len_raw);
+        let len = u64::from_le_bytes(len_raw);
 
-    if len > MAX_MSG_LEN {
-        bail!("Message is too long (length: {} bytes)", len);
-    }
-
-    let mut buf = vec![0; len.try_into().unwrap()];
-    if let Err(err) = stream.read_exact(&mut buf).await {
-        if err.kind() == std::io::ErrorKind::UnexpectedEof {
-            log::warn!("receive tcp message failed: {}", err);
-            return Ok(None);
-        } else {
-            return Err(eyre::Error::new(err).wrap_err("failed to read message"));
+        if len > MAX_MSG_LEN {
+            bail!("Message is too long (length: {} bytes)", len);
         }
+
+        let mut buf = vec![0; len.try_into().unwrap()];
+        if let Err(err) = stream.read_exact(&mut buf).await {
+            if err.kind() == std::io::ErrorKind::UnexpectedEof {
+                log::warn!("receive tcp message failed: {}", err);
+                return Ok(None);
+            } else {
+                return Err(eyre::Error::new(err).wrap_err("failed to read message"));
+            }
+        }
+        serde_json::from_slice(&buf)
+            .with_context(|| {
+                format!(
+                    "failed to deserialize message: `{}`",
+                    String::from_utf8_lossy(&buf)
+                )
+            })
+            .map(Some)
     }
-    serde_json::from_slice(&buf)
-        .with_context(|| {
-            format!(
-                "failed to deserialize message: `{}`",
-                String::from_utf8_lossy(&buf)
-            )
-        })
-        .map(Some)
 }
