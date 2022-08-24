b7d86ce48e8b7d3d6bb8ed26a8a3b50bf91a2725
Tricster <mediosrity@gmail.com>
2022-08-24 21:26:28 +0800

    Add mysql driver examples.
    Signed-off-by: Tricster <mediosrity@gmail.com>

---
+++ mysql/src/bin/query.rs
@@ -0,0 +1,30 @@
+use mysql::prelude::*;
+use mysql::*;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Db {
+    host: String,
+    db: String,
+}
+
+fn main() -> std::result::Result<(), Error> {
+    let url = if let Ok(url) = std::env::var("DATABASE_URL") {
+        let opts = Opts::from_url(&url).expect("DATABASE_URL invalid");
+        if opts
+            .get_db_name()
+            .expect("a database name is required")
+            .is_empty()
+        {
+            panic!("database name is empty");
+        }
+        url
+    } else {
+        "mysql://root:password@127.0.0.1:3306/mysql".to_string()
+    };
+    let pool = Pool::new(url.as_str())?;
+    let mut conn = pool.get_conn()?;
+
+    let selected_dbs = conn.query_map("SELECT Host, Db from db;", |(host, db)| Db { host, db })?;
+    dbg!(selected_dbs);
+    Ok(())
+}
