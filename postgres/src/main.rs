0cdaf7f253864734aba898c56a670167be2bfb05
Michael Yuan <michael@secondstate.io>
2023-02-10 23:02:06 -0600

    Fix postgres docker
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- postgres/src/main.rs
+++ postgres/src/main.rs
@@ -45,7 +45,6 @@ impl Order {
 async fn main() -> Result<(), Error> {
     // Connect to the database.
     let (client, connection) = tokio_postgres::connect(&*get_url(), NoTls).await?;
-    eprintln!("1");
 
     // The connection object performs the actual communication with the database,
     // so spawn it off to run on its own.
@@ -54,11 +53,8 @@ async fn main() -> Result<(), Error> {
             eprintln!("connection error: {}", e);
         }
     });
-    eprintln!("2");
 
-    /*
-    client.execute("CREATE TABLE orders (order_id INT, production_id INT, quantity INT, amount FLOAT, shipping FLOAT, tax FLOAT, shipping_address VARCHAR(256));", &[]).await?;
-    eprintln!("3");
+    client.execute("CREATE TABLE IF NOT EXISTS orders (order_id INT, production_id INT, quantity INT, amount REAL, shipping REAL, tax REAL, shipping_address VARCHAR(256));", &[]).await?;
 
     let orders = vec![
         Order::new(1, 12, 2, 56.0, 15.0, 2.0, String::from("Mataderos 2312")),
@@ -67,29 +63,39 @@ async fn main() -> Result<(), Error> {
         Order::new(4, 8, 8, 126.0, 20.0, 12.0, String::from("224 Pandan Loop")),
         Order::new(5, 24, 1, 46.0, 10.0, 2.0, String::from("No.10 Jalan Besar")),
     ];
-    eprintln!("4");
 
     for order in orders.iter() {
         client.execute(
             "INSERT INTO orders (order_id, production_id, quantity, amount, shipping, tax, shipping_address) VALUES ($1, $2, $3, $4, $5, $6, $7)",
             &[&order.order_id, &order.production_id, &order.quantity, &order.amount, &order.shipping, &order.tax, &order.shipping_address]
         ).await?;
-        eprintln!("5x");
     }
 
     let rows = client.query("SELECT * FROM orders;", &[]).await?;
-    println!("{:#?}", rows);
-    */
+    for row in rows.iter() {
+        let order_id : i32 = row.get(0);
+        println!("order_id {}", order_id);
 
-    // Now we can execute a simple statement that just returns its parameter.
-    let rows = client
-        .query("SELECT $1::TEXT", &[&"hello WasmEdge"])
-        .await?;
+        let production_id : i32 = row.get(1);
+        println!("production_id {}", production_id);
 
-    // And then check that we got back the same string we sent over.
-    let value: &str = rows[0].get(0);
-    println!("{}", value);
-    // assert_eq!(value, "hello world");
+        let quantity : i32 = row.get(2);
+        println!("quantity {}", quantity);
+
+        let amount : f32 = row.get(3);
+        println!("amount {}", amount);
+
+        let shipping : f32 = row.get(4);
+        println!("shipping {}", shipping);
+
+        let tax : f32 = row.get(5);
+        println!("tax {}", tax);
+
+        let shipping_address : &str = row.get(6);
+        println!("shipping_address {}", shipping_address);
+    }
+
+    client.execute("DELETE FROM orders;", &[]).await?;
 
     Ok(())
 }
